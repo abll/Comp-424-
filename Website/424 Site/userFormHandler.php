8929ccc5341a834c6b896baaed2bceae1150b3d3
abll <abellawal@yahoo.com>
2015-05-02 10:10:04 -0700

    Connecting to the data base
    Now the form connects to the data base i am still having an input type
    error with the values of the secret questions

--- Website/424 Site/userFormHandler.php
+++ Website/424 Site/userFormHandler.php
@@ -1,12 +1,16 @@
 <?php
+    
         
 $firstName = htmlspecialchars($_POST['userFirstName']);
 $lastName = htmlspecialchars($_POST['userLastName']);
 $uName = htmlspecialchars($_POST['userName']);
+$userBirthDate = htmlspecialchars($_POST['userDOB']);
 $emailAddress = htmlspecialchars($_POST['userEmailAddress']);
 $pWord = htmlspecialchars($_POST['userPassword']);
-$secretQuestion = htmlspecialchars($_POST['secQues1']);
-$secretQuestionAnswer = htmlspecialchars($_POST['secQues1Answer']);
+$secretQuestion1 = (int)htmlspecialchars($_POST['secQues1']);
+$secretQuestion1Answer = htmlspecialchars($_POST['secQues1Answer']);
+$secretQuestion2 = (int)htmlspecialchars($_POST['secQues2']);
+$secretQuestion2Answer = htmlspecialchars($_POST['secQues2Answer']);
 
 
 print "First Name: $firstName";
@@ -14,9 +18,37 @@ print "\nLast Name: $lastName";
 print "\nUser Name: $uName";
 print "\nEmail Address: $emailAddress";
 print "\nPassWord: $pWord";
-print "Secret Question 1: $secretQuestion";
-print "Secret Question Answer 1: $secretQuestionAnswer";
+print "Secret Question 1: $secretQuestion1";
+print "Secret Question Answer 1: $secretQuestion1Answer";
+print "Secret Question 2: $secretQuestion2";
+print "Secret Question Answer 1: $secretQuestion2Answer";
 
+//Check into Secret Question Values Data Entry maybe try changing input type
+
+$servername = "localhost";
+$dbUserName = "root";
+$dbPWord = "";
+$dbName = "userdatabase";
+
+$conn = new mysqli($servername, $dbUserName, $dbPWord, $dbName);
+
+if($conn->connect_error)
+{
+    die("Connection Failed: ".$conn->connect_error);
+}
+
+echo "\nConnected Successfully";
+
+$insertQuery  = "INSERT INTO `userdatabase`.`users` (`user_name`, `first_name`, `last_name`, `Date_of_Birth`, `user_email`, `password`, `sq_1_ID`, `sq_1_Answer`, `sq_2_ID`, `sq_2_answer`) 
+VALUES ('$uName', '$firstName', '$lastName', '$userBirthDate', '$emailAddress', '$pWord', '$secretQuestion1', '$secretQuestion1Answer', '$secretQuestion2', '$secretQuestion2Answer')";
+
+if ($conn->query($insertQuery) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $insertQuery . "<br>" . $conn->error;
+}
+
+$conn->close();
 /*Another day look up String Ouput Info*/
 
 ?>
